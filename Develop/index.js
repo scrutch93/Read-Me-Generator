2b11de3ebdc32734f29cd56a5aa081c00d95ae9d
Steven Crutchfield <stevccru@icloud.com>
2021-06-01 17:48:01 -0400

    edited user questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,17 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
-        {
+         {
+           type: 'input',
+           name: 'Title',
+           message: 'Enter name of Project?',
+         },
+
+          {
             type: 'input',
             name: 'Description',
             message: 'Describe your project',
@@ -23,12 +30,17 @@ const questions = () => {
           {
             type: 'input',
             name: 'Contributing',
-            message: 'What is your favorite food?',
+            message: 'Would you like to contribute to this project?',
           },
           {
             type: 'input',
             name: 'Tests',
-            message: 'Intersted in contributing?',
+            message: 'What tests have been run on this project?',
+          },
+          {
+            type: 'input',
+            name: 'Questions',
+            message: 'Please list any questions or concerns regarding this project?',
           },
          
 
